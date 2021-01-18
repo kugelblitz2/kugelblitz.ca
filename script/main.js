e95da7c2d6c407dd015a4bec48280962fad8764e
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2021-01-18 02:41:30 -0500

    added dark mode

---
+++ script/main.js
@@ -0,0 +1,24 @@
+function scrollDown(direction) {    // direction = 1 scrolls down, direction = -1 scrolls up
+    var pageHeight = direction * document.getElementsByClassName("page")[0].clientHeight;
+    window.scrollBy(0, pageHeight);
+}
+
+// Dark Mode
+var light = '#F9F9F9';
+var dark = '#444444';
+setColorScheme();
+
+if(window.matchMedia){
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setColorScheme);
+}
+
+function setColorScheme() {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        document.documentElement.style.setProperty('--maincolor', dark);
+        document.documentElement.style.setProperty('--textcolor', light);
+    }
+    else{
+        document.documentElement.style.setProperty('--maincolor', light);
+        document.documentElement.style.setProperty('--textcolor', dark);
+    }
+}
