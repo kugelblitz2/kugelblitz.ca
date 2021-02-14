2d7bb3c44c803ec768a3ab3f64b0713e65fd4a4f
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2021-02-14 03:32:54 -0500

    Added link highlighting on hover

--- script/main.js
+++ script/main.js
@@ -7,6 +7,8 @@ function scrollDown(direction) {    // direction = 1 scrolls down, direction = -
 // Dark Mode
 var light = '#F9F9F9';
 var dark = '#444444';
+var lighthover = '#717171';
+var darkhover = '#C3C3C3';
 setColorScheme();
 
 if(window.matchMedia){
@@ -17,9 +19,11 @@ function setColorScheme() {
     if(window.matchMedia('(prefers-color-scheme: dark)').matches){
         document.documentElement.style.setProperty('--maincolor', dark);
         document.documentElement.style.setProperty('--textcolor', light);
+        document.documentElement.style.setProperty('--hover', darkhover);
     }
     else{
         document.documentElement.style.setProperty('--maincolor', light);
         document.documentElement.style.setProperty('--textcolor', dark);
+        document.documentElement.style.setProperty('--hover', lighthover);
     }
 }
